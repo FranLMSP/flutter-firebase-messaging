67448d69b1d55aeb6e0e1300c7790892e0f90d85
Franco Colmenarez <francoacg1@gmail.com>
2021-02-15 13:24:23 -0500

    Message input

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../widgets/messages/messages.dart';
+import '../widgets/messages/new_message.dart';
+
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -36,29 +39,15 @@ class ChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: StreamBuilder(
-        stream: Firestore.instance.collection('chats/DKuF7M1se9cQPOgGHBCf/messages').snapshots(),
-        builder: (ctx, streamSnapshot) {
-          if (streamSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator(),);
-          }
-          final documents = streamSnapshot.data.documents;
-          return ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: (ctx, index) => Container(
-              padding: const EdgeInsets.all(8),
-              child: Text(documents[index]['text']),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Messages(),
             ),
-          );
-        }
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Firestore.instance.collection('chats/DKuF7M1se9cQPOgGHBCf/messages').add({
-            'text': 'Dummy message',
-          });
-        },
+            NewMessage(),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/messages/new_message.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class NewMessage extends StatefulWidget {
+  @override
+  _NewMessageState createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  String _enteredMessage = '';
+  final _controller = TextEditingController();
+
+  void _sendMessage() {
+    FocusScope.of(context).unfocus();
+    Firestore.instance.collection('chat').add({
+      'text': _enteredMessage,
+      'createdAt': Timestamp.now(),
+    });
+    _controller.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 8),
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(labelText: 'Send a message...'),
+              onChanged: (value) => setState(() => _enteredMessage = value.trim()),
+            ),
+          ),
+          IconButton(
+            color: Theme.of(context).primaryColor,
+            icon: Icon(Icons.send),
+            onPressed: _enteredMessage.isEmpty ? null : _sendMessage,
+          ),
+        ],
+      ),
+      
+    );
+  }
+}
