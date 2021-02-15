238ddcad22480a08a450e8235852032cec3980e7
Franco Colmenarez <francoacg1@gmail.com>
2021-02-15 12:43:48 -0500

    Logout logic

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter chat'),
+        actions: <Widget>[
+          DropdownButton(
+            onChanged: (identifier) {
+              if (identifier == 'logout') {
+                FirebaseAuth.instance.signOut();
+              }
+            },
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+            items: [
+              DropdownMenuItem(
+                value: 'logout',
+                child: Container(
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.exit_to_app),
+                      SizedBox(width: 8),
+                      Text('Logout'),
+                    ]
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
       body: StreamBuilder(
         stream: Firestore.instance.collection('chats/DKuF7M1se9cQPOgGHBCf/messages').snapshots(),
         builder: (ctx, streamSnapshot) {
