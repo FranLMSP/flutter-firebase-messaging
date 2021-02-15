fbb0d313ae9b1d5231b496aa860ef12e86933469
Franco Colmenarez <francoacg1@gmail.com>
2021-02-15 14:03:57 -0500

    Styling messages

--- lib/widgets/messages/messages.dart
+++ lib/widgets/messages/messages.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+import './message_bubble.dart';
 
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Firestore.instance.collection('chat').orderBy('createdAt', descending: true).snapshots(),
-      builder: (ctx, chatSnapshot) {
-        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+    return FutureBuilder(
+      future: FirebaseAuth.instance.currentUser(),
+      builder: (ctx, futureSnapshot) {
+        if (futureSnapshot.connectionState == ConnectionState.waiting) {
           return Center(child: CircularProgressIndicator(),);
         }
-        final messages = chatSnapshot.data.documents;
-        return ListView.builder(
-          reverse: true,
-          itemCount: messages.length,
-          itemBuilder: (ctx, index) => Text(messages[index]['text']),
+        return StreamBuilder(
+          stream: Firestore.instance.collection('chat').orderBy('createdAt', descending: true).snapshots(),
+          builder: (ctx, chatSnapshot) {
+            if (chatSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator(),);
+            }
+            final messages = chatSnapshot.data.documents;
+            return ListView.builder(
+              reverse: true,
+              itemCount: messages.length,
+              itemBuilder: (ctx, index) => MessageBubble(
+                messages[index]['text'],
+                messages[index]['userId'] == futureSnapshot.data.uid,
+                key: ValueKey(messages[index].documentID),
+              ),
+            );
+          },
         );
-      }
+      },
     );
   }
 }
